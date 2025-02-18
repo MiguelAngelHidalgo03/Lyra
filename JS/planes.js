dd223781f2bb8499c6aebde1ff5588ed468ef936
ZetaxD <juanalexanderchango@gmail.com>
2025-02-18 13:50:49 +0100

    Update planes.js

--- JS/planes.js
+++ JS/planes.js
@@ -14,3 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('.anounce').classList.add('show');
     }, 80); 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const controlButtons = document.querySelectorAll('.control-btn img');
+
+    controlButtons.forEach(button => {
+        const originalSrc = button.src;
+        const hoverSrc = originalSrc.replace('.png', '-hover.png'); // Asegúrate de tener las imágenes con el sufijo '-hover'
+
+        button.addEventListener('mouseover', () => {
+            button.src = hoverSrc;
+        });
+
+        button.addEventListener('mouseout', () => {
+            button.src = originalSrc;
+        });
+    });
+});
